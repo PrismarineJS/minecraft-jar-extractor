810f5fda0502a2c49967e41c41f03f38594f3767
redcarti <ppamperr@gmail.com>
2020-07-14 18:42:55 +0500

    multiple ver and langs

--- lang.js
+++ lang.js
@@ -1,33 +1,52 @@
 const LauncherDownload = require('minecraft-wrap').LauncherDownload
 const path = require('path')
 const fs = require('fs')
-if (!(process.argv.length >= 4 && process.argv.length <= 5)) {
-  console.log('Usage : node lang.js <version> <output_dir>')
+const { version } = require('punycode')
+if (!(process.argv.length >= 5 && process.argv.length <= 6)) {
+  console.log('Usage : node lang.js <version1,version2,...> <lang1,lang2,...>|* <output_dir>')
   process.exit(0)
 }
 
-const version = process.argv[2]
+const versions = process.argv[2].split(',')
 const dir = path.join(process.env.APPDATA, '/.minecraft')
 const os = process.platform
-const dirToSave = process.argv[3] || version
+const langs = process.argv[3] !== '*' ? process.argv[3].split(',') : '*'
+const dirToSave = process.argv[4]
 
 const ld = new LauncherDownload(dir, os)
 
 if (!fs.existsSync(path.join(__dirname, dirToSave))) {
   fs.mkdirSync(path.join(__dirname, dirToSave), (err) => { console.error(err) })
 }
-
-ld.getAssetIndex(version).then(assets => {
-  Object.keys(assets.objects).forEach(asset => {
-    if (/^(minecraft\/lang\/)/.test(asset)) {
-      ld.getAsset(asset, version).then(r => {
-        fs.readFile(r, { encoding: 'utf8' }, (err, data) => {
-          if (err) console.error(err)
-          fs.writeFileSync(path.join(__dirname, dirToSave + '/' + asset.split('/')[2]), data)
-        })
-      })
-    }
+let langi = 0
+let assets1 = []
+versions.forEach(version => {
+  ld.getAssetIndex(version).then(assets => {
+    Object.keys(assets.objects).forEach(asset => {
+      if (/^(minecraft\/lang\/)/.test(asset)) {
+        if (langs !== '*') {
+          if (langs[langi] === asset.split('/')[2].split('.')[0]) {
+            assets1.push(asset); langi++
+          }
+        } else assets1.push(asset)
+      }
+    })
+  
+    downloadLangs(version)
+  }).catch(err => {
+    console.log(err)
   })
-}).catch(err => {
-  console.log(err)
+
+  setTimeout(()=>{}, 500)
 })
+
+const downloadLangs = (version) => {
+  assets1.forEach(asset => {
+    ld.getAsset(asset, version).then(r => {
+      fs.readFile(r, { encoding: 'utf8' }, (err, data) => {
+        if (err) console.error(err)
+        fs.writeFileSync(path.join(__dirname, dirToSave + '/' + asset.split('/')[2]), data)
+      })
+    })
+  })
+}
