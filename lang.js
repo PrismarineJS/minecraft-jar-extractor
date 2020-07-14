3501b283a9d8c524421c6adf2a1df849d7786567
redcarti <55404819+redcarti@users.noreply.github.com>
2020-07-14 17:42:41 +0500

    Update lang.js

--- lang.js
+++ lang.js
@@ -1,62 +1,31 @@
-'use strict'
-
-const fs = require('fs-extra')
+const LauncherDownload = require('minecraft-wrap').LauncherDownload
 const path = require('path')
-const getMinecraftFiles = require('./get_minecraft_files')
-
-if (process.argv.length !== 5) {
-  console.log('Usage : node lang.js <version1,version2,...> <output_dir> <temporary_dir>')
-  process.exit(1)
+const fs = require('fs')
+if (!(process.argv.length >= 3 && process.argv.length <= 4)) {
+  console.log('Usage : node lang.js <version>')
+  process.exit(0)
 }
 
-const minecraftVersions = process.argv[2].split(',')
-const outputDir = path.resolve(process.argv[3])
-const temporaryDir = path.resolve(process.argv[4])
+const version = process.argv[2]
+const dir = path.join(process.env.APPDATA, '/.minecraft')
+const os = process.platform
 
-minecraftVersions.forEach(minecraftVersion => {
-  extract(minecraftVersion, outputDir + '/' + minecraftVersion, temporaryDir, function (err) {
-    if (err) {
-      console.log(err.stack)
-      return
+const ld = new LauncherDownload(dir, os)
+
+if (!fs.existsSync(path.join(__dirname, version))){
+  fs.mkdirSync(path.join(__dirname, version), (err)=>{});
+}
+
+ld.getAssetIndex(version).then(assets => {
+  Object.keys(assets['objects']).forEach(asset => {
+    if(/^(minecraft\/lang\/)/.test(asset)) {
+      ld.getAsset(asset, version).then(r => {
+        fs.readFile(r, {encoding: 'utf8'}, (err, data) => {
+          fs.writeFileSync(path.join(__dirname, version+'/'+asset.split('/')[2]), data)
+        })
+      })
     }
-    console.log('done ' + minecraftVersion + '!')
-  })
+  });
+}).catch(err => {
+  console.log(err)
 })
-
-function copyLang (unzippedFilesDir, outputDir) {
-  try {
-    fs.copySync(unzippedFilesDir + '/assets/minecraft/lang/en_US.lang', outputDir + '/en_us.lang')
-  } catch (err) {
-    try {
-      fs.copySync(unzippedFilesDir + '/assets/minecraft/lang/en_us.lang', outputDir + '/en_us.lang')
-    } catch (err) {
-      fs.copySync(unzippedFilesDir + '/assets/minecraft/lang/en_us.json', outputDir + '/en_us.json')
-    }
-  }
-}
-
-function parseLang (outputDir) {
-  if (fs.existsSync(outputDir + '/en_us.json')) {
-    return
-  }
-  const lang = {}
-  fs.readFileSync(outputDir + '/en_us.lang', 'utf8').split('\n').forEach(function (l) {
-    const c = l.split(/=(.+)/)
-    if (c.length === 3) lang[c[0]] = c[1]
-  })
-
-  fs.writeFileSync(outputDir + '/en_us.json', JSON.stringify(lang, null, 2))
-}
-
-function extract (minecraftVersion, outputDir, temporaryDir, cb) {
-  getMinecraftFiles(minecraftVersion, temporaryDir, function (err, unzippedFilesDir) {
-    if (err) {
-      cb(err)
-      return
-    }
-    fs.mkdirpSync(outputDir)
-    copyLang(unzippedFilesDir, outputDir)
-    parseLang(outputDir)
-    cb()
-  })
-}
