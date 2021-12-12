b8b692fc81bcfb5bb482bc90065120780a34a227
u9g <aibotted@gmail.com>
2021-12-12 17:58:58 +0000

    add conditional cli prefix depending on version to support older version

--- extract_data_from_minecraft.js
+++ extract_data_from_minecraft.js
@@ -3,13 +3,16 @@ const downloadServer = promisify(require('minecraft-wrap').downloadServer)
 const fs = require('fs').promises
 const { execSync } = require('child_process')
 const path = require('path')
+const mcData = require('minecraft-data')
 const OUT_DIR = 'minecraft_extracted_data'
 const rm = async path => fs.rm(path, { force: true, recursive: true })
-module.exports = async (version) => {
+const a = async (version) => {
   try { await fs.mkdir(OUT_DIR) } catch (e) {}
   try { await rm('server.jar') } catch (e) {}
   await downloadServer(version, path.join(OUT_DIR, 'server.jar'))
-  const output = execSync(`java -DbundlerMainClass=net.minecraft.data.Main -jar ${path.join(OUT_DIR, 'server.jar')} --reports`).toString()
+  const isNewerThan118 = mcData(version).isNewerOrEqualTo('1.18')
+  const cliStartCommand = isNewerThan118 ? `java -DbundlerMainClass=net.minecraft.data.Main -jar ${path.join(OUT_DIR, 'server.jar')}` : `java -cp ${path.join(OUT_DIR, 'server.jar')} net.minecraft.data.Main`
+  const output = execSync(`${cliStartCommand} --reports`).toString()
   if (!output.includes('All providers took:')) {
     console.log('Server dumping failed, output printed:')
     console.log(output)
@@ -22,3 +25,5 @@ module.exports = async (version) => {
     await rm(path.join(OUT_DIR, 'server.jar'))
   } catch (e) {}
 }
+
+a('1.18')
